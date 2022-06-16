e2cb6852f9e7f38b9e8bf68ad2469e9a412ce00f
KIES Akram-Walid <akram-walid.kies@globalis-ms.com>
2022-06-16 09:59:51 +0200

    Change README

--- src/ValidateWebSiteCommand.php
+++ src/ValidateWebSiteCommand.php
@@ -1,4 +1,5 @@
 <?php
+
 // src/Command/CreateUserCommand.php
 namespace Globalis\SeoHnTagValidator;
 
@@ -18,7 +19,6 @@ use Symfony\Component\Console\Output\OutputInterface;
 )]
 class ValidateWebSiteCommand extends Command
 {
-
     public function __construct()
     {
         parent::__construct();
@@ -26,23 +26,27 @@ class ValidateWebSiteCommand extends Command
 
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        $io = new SymfonyStyle($input,$output);
+        $io = new SymfonyStyle($input, $output);
         $validator  = new \Globalis\SeoHnTagValidator\SeoHnTagValidator();
 
         $concurrent = 3;
         $errors = 1;
 
-        if(!$input->getOption('only-errors')) $errors = 0;
-        if($input->getOption('concurrent-requests')) $concurrent = $input->getOption('concurrent-requests');
+        if (!$input->getOption('only-errors')) {
+            $errors = 0;
+        }
+        if ($input->getOption('concurrent-requests')) {
+            $concurrent = $input->getOption('concurrent-requests');
+        }
 
-        
 
-        $res = $validator->validateWebSite($input->getArgument('url'),$errors,$concurrent);
+
+        $res = $validator->validateWebSite($input->getArgument('url'), $errors, $concurrent);
 
         //convert array errors to string
         $table = [];
         $countErrors = 0;
-        for ($i=0; $i < count($res); $i++) { 
+        for ($i = 0; $i < count($res); $i++) {
             $errors = "";
             $tags = "";
             foreach ($res[$i]["errors"] as $value) {
@@ -50,23 +54,26 @@ class ValidateWebSiteCommand extends Command
                 $countErrors++;
             }
             foreach ($res[$i]["tags"] as $value) {
-                $tags = $tags . "<" .$value["tag"] . ">" . $value["value"] . "</" .$value["tag"] . ">" . "\n";
+                $tags = $tags . "<" . $value["tag"] . ">" . $value["value"] . "</" . $value["tag"] . ">" . "\n";
             }
             $table[$i]["url"] = $res[$i]["url"];
             $table[$i]["is_valid"] = $res[$i]["is_valid"];
             $table[$i]["errors"] = $errors;
-            if($tags === "") $table[$i]["tags"] = "empty tags";
-            else $table[$i]["tags"] = $tags;
+            if ($tags === "") {
+                $table[$i]["tags"] = "empty tags";
+            } else {
+                $table[$i]["tags"] = $tags;
+            }
         }
 
         $io->table(["url","errors","is_valid","tags"], $table);
 
-        if($countErrors === 0)
-            $io->success($input->getArgument('url')." crawled successfully (0 errors)");
-        else   
-            $io->error($input->getArgument('url')." crawled with ".$countErrors." error(s)");
+        if ($countErrors === 0) {
+            $io->success($input->getArgument('url') . " crawled successfully (0 errors)");
+        } else {
+            $io->error($input->getArgument('url') . " crawled with " . $countErrors . " error(s)");
+        }
         return Command::SUCCESS;
-
     }
 
     protected function configure(): void
@@ -75,7 +82,7 @@ class ValidateWebSiteCommand extends Command
             // the command help shown when running the command with the "--help" option
             ->setHelp('This command allows you to run hN validator in the whole website')
             ->addArgument('url', InputArgument::REQUIRED, 'website url')
-            ->addOption('only-errors', 'e', InputOption::VALUE_OPTIONAL,"displaying only urls with errors")
+            ->addOption('only-errors', 'e', InputOption::VALUE_OPTIONAL, "displaying only urls with errors")
             ->addOption('concurrent-requests', 'c', InputOption::VALUE_OPTIONAL)
         ;
     }
